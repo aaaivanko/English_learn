4aebed12aa66b367e2a4581f1afdd482dd097c34
Ivan Chvartatskyy <ivan.chvartatskyy@gmail.com>
2023-11-01 20:56:44 +0100

    Added sceleton for working and cons file

---
+++ english_months/main.py
@@ -0,0 +1,12 @@
+import random
+from constants import english_mn, ukraine_mn
+
+
+random_number = random.randint(1, 12)
+random_ukr_month = ukraine_mn[random_number]
+
+print(random_ukr_month)
+
+
+
+
