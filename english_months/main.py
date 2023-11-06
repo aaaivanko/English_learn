81a1c3de94985e909527f34ef3b00cd5b6c320d3
Ivan Chvartatskyy <ivan.chvartatskyy@gmail.com>
2023-11-06 10:53:51 +0100

    added month program funstion for the checking knowledge

--- english_months/main.py
+++ english_months/main.py
@@ -1,12 +1,29 @@
 import random
-from constants import english_mn, ukraine_mn
+from constants import english_mn
 
 
-random_number = random.randint(1, 12)
-random_ukr_month = ukraine_mn[random_number]
+def months_program():
+    print(f"Welcome to the months program."
+          f"\nHere I would like to check your knowledge of english months. ")
+    active = True
 
-print(random_ukr_month)
+    while active:
+        random_number = random.randint(1, 12)
+        random_eng_month = english_mn[random_number]
+        user_answer = input(f"Please write name of number that is {random_number}: "
+                            f"\n If you want to finish the program please press `q`: ")
+        if user_answer != 'q':
+            if random_eng_month.lower() != user_answer.lower():
+                print(f"Sorry, your answer {user_answer} is wrong"
+                      f"\n The correct answer is: {random_eng_month}. Please rty again.")
+            else:
+                print(f"Your answer {user_answer} is correct it's indeed {random_number} month")
+        else:
+            active = False
+            return f"Thank you for the game. Goodbuy!"
 
+
+print(months_program())
 
 
 
